9e7f3129b90142418c83dc678dc1db3389ab932a
Steven Richardson <sarichardson@clugon.home>
2015-08-05 16:34:36 +0100

    Add the index file again

---
+++ public/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Magestead - A Vagrant LEMP Stack for Magento Developers</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="http://magestead.richdynamix.com/css/bootstrap.min.css" type="text/css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="http://magestead.richdynamix.com/css/creative.css" type="text/css">
+</head>
+
+<body id="page-top">
+
+<nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand page-scroll" href="#page-top">Magestead</a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="menu">
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a class="page-scroll" href="http://magestead.richdynamix.com/#about">About</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="http://magestead.richdynamix.com/#box">Box</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="http://magestead.richdynamix.com/#stack">Stack</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="http://magestead.richdynamix.com/#docs">Docs</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="http://magestead.richdynamix.com/#download">Download</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container-fluid -->
+</nav>
+
+<header>
+    <div class="header-content">
+        <div class="header-content-inner">
+            <h1>A Vagrant LEMP Stack for Magento Developers</h1>
+            <hr>
+            <p>Boostrap your Magento development painlessly!</p>
+            <a href="http://magestead.richdynamix.com/#box" class="btn btn-primary btn-xl page-scroll">Find Out More</a>
+        </div>
+    </div>
+</header>
+
+<!-- jQuery -->
+<script src="http://magestead.richdynamix.com/js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="http://magestead.richdynamix.com/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
