4807f676dff22cb4e78168dce0fe05d00fb94b6e
Steven Richardson <sarichardson@clugon.home>
2015-08-05 08:07:05 +0100

    Add symfony bootstrap server blocks

--- provision/after.sh
+++ provision/after.sh
@@ -2,4 +2,4 @@
 
 # If you would like to do some extra provisioning you may
 # add any commands you wish to this file and they will
-# be run after the Homestead machine is provisioned.
+# be run after the Magestead machine is provisioned.
