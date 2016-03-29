7dcd66056b92ca337c942ba77848984cc955c938
Steven Richardson <srichardson@sessiondigital.com>
2016-03-29 16:53:45 +0100

    Add apache configuration

---
+++ puphpet/magestead/magento2/configure-apache.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+APP_NAME=${1};
+DIR=${2};
+BASE_URL=${3};
+
+vhost="
+# ************************************
+# Vhost template applied during provision
+# Managed by Magestead
+# ************************************
+<VirtualHost *:80>
+
+  DocumentRoot \"$DIR/magento2/pub\"
+
+  <Directory \"$DIR/magento2/pub\">
+    Options Indexes FollowSymLinks
+    AllowOverride All
+    Require all granted
+    Order allow,deny
+    allow from all
+
+    <FilesMatch \"\.php\$\">
+      Require all granted
+      SetHandler proxy:fcgi://127.0.0.1:9090
+    </FilesMatch>
+  </Directory>
+
+  ErrorLog \"/var/log/httpd/$BASE_URL_error.log\"
+  LogLevel warn
+  CustomLog \"/var/log/httpd/$BASE_URL_access.log\" combined
+
+</VirtualHost>
+"
+
+sudo echo "$vhost" > "/etc/httpd/conf.d/$APP_NAME.conf"
+sudo service httpd restart
+sudo service php-fpm restart
+
+
