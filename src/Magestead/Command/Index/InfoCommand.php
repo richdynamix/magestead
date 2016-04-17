179a1def1f9ad17a2f1cee39157b7a6068c5de2d
Steven Richardson <srichardson@sessiondigital.com>
2016-04-17 22:01:37 +0100

    Add index management commands

---
+++ src/Magestead/Command/Index/InfoCommand.php
@@ -0,0 +1,54 @@
+<?php namespace Magestead\Command\Index;
+
+use Magestead\Command\ProcessCommand;
+use Magestead\Helper\Config;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class InfoCommand extends Command
+{
+    protected $_config;
+    protected $_projectPath;
+
+    protected function configure()
+    {
+        $this->_projectPath = getcwd();
+        $this->setName("index:info");
+        $this->setDescription("Show available indexes");
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return ProcessCommand
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('<info>Getting index information</info>');
+
+        $command = $this->getCommand(new Config($output));
+        $passedCommand = "vagrant ssh -c '". $command ."'";
+        return new ProcessCommand($passedCommand, $this->_projectPath, $output);
+    }
+
+    /**
+     * @param Config $config
+     * @return bool|string
+     */
+    protected function getCommand(Config $config)
+    {
+        $type = $config->type;
+        switch ($type) {
+            case 'magento':
+                // todo add magerun commands
+                return "";
+                break;
+            case 'magento2':
+                return "cd /var/www/public;bin/magento indexer:info";
+                break;
+        }
+
+        return false;
+    }
+}
